147d4f7d68902ecff83209cbbb30698fa65fc426
AppDevNotes <anushkamadusanka2016@gmail.com>
2020-04-25 23:00:25 -0700

    Initial commit

---
+++ app/src/main/java/com/anushka/coroutinesdemo1/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.anushka.coroutinesdemo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    private var count = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnCount.setOnClickListener {
+            tvCount.text = count++.toString()
+
+        }
+        btnDownloadUserData.setOnClickListener {
+
+            CoroutineScope(Dispatchers.Main).launch {
+               //tvUserMessage.text = UserDataManager().getTotalUserCount().toString()
+               tvUserMessage.text = UserDataManager2().getTotalUserCount().toString()
+            }
+
+        }
+    }
+
+    private fun downloadUserData() {
+        for (i in 1..200000) {
+            Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
+        }
+    }
+}
